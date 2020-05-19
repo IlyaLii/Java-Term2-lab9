6ae91cc6f4a50cd048c1770e884b6134b97ffc5b
Ilya <agronat88@icloud.com>
2020-05-20 01:15:18 +0300

    add packages

---
+++ src/servlet/StartupServlet.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import bsu.rfe.java.group7.lab9.Truhov.varA1.entity.Ad;
+import bsu.rfe.java.group7.lab9.Truhov.varA1.entity.AdList;
+import bsu.rfe.java.group7.lab9.Truhov.varA1.entity.UserList;
+import bsu.rfe.java.group7.lab9.Truhov.varA1.helper.AdListHelper;
+import bsu.rfe.java.group7.lab9.Truhov.varA1.helper.UserListHelper;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+public class StartupServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        UserList userList = UserListHelper.readUserList(getServletContext());
+
+        getServletContext().setAttribute("users", userList);
+
+        AdList adList = AdListHelper.readAdList(getServletContext());
+
+        getServletContext().setAttribute("ads", adList);
+        for (Ad ad: adList.getAds()) {
+            ad.setAuthor(userList.findUser(ad.getAuthorId()));
+            ad.setLastModified(ad.getLastModified());
+        }
+    }
+}
